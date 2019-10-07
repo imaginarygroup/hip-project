c18116c0daca65c815d69024245f149bf7ea68e5
jason.lu <jason-lu.mbp>
2019-10-08 00:49:52 +0800

    Added training changes

--- training.py
+++ training.py
@@ -2,7 +2,7 @@ import os
 from random import random
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, BatchNormalization, Dropout
 # Generate dummy data
 import numpy as np
 # For custom metrics
@@ -11,7 +11,9 @@ import scipy.io
 
 class HipJointV1(object):
 
+    MINIMUM_FEATURE_POINT_COUNT = 50
     FIT_RATE = 0.75
+    EPOCH_TRIES = 1200
 
     def __init__(self, insize, outsize):
         self.input_size = insize
@@ -38,22 +40,24 @@ class HipJointV1(object):
         :return:
         """
         model = Sequential()
-        model.add(Dense(32, input_dim=self.input_size))
-        model.add(Activation('relu'))
-        model.add(Dense(32, input_shape=(self.input_size,)))
-        model.add(Activation('relu'))
-        model.add(Dense(10))
-        model.add(Activation('softmax'))
+        model.add(Dense(self.input_size, input_dim=self.input_size))
+        model.add(Activation('tanh'))
+        model.add(BatchNormalization(momentum=0.99))
+        model.add(Dense(int(self.input_size / 2), input_shape=(self.input_size,)))
+        model.add(Activation('tanh'))
+        model.add(Dropout(rate=0.1))
+        model.add(Dense(int(self.input_size / 20)))
+        model.add(Activation('tanh'))
         model.add(Dense(self.output_size))
-        model.add(Activation('relu'))
+        model.add(Activation('softmax'))
         model.compile(optimizer='rmsprop',
                       loss='mse')
 
         return model
 
-    def fit_model(self, model, data, labels):
+    def fit_model(self, model, data, labels, epoch_times=10):
         # Train the model, iterating on the data in batches of 32 samples
-        model.fit(data, labels, epochs=10, batch_size=32)
+        model.fit(data, labels, epochs=epoch_times, batch_size=32)
 
 
     def mean_pred(y_true, y_pred):
@@ -123,12 +127,12 @@ class HipJointV1(object):
         a = res.view(dtype)  # create a structured array
         a = a.reshape((a.shape[0], ))
         sort_res = np.sort(a, order='radius')
-        if sort_res.shape[0] <= 50:
+        if sort_res.shape[0] <= HipJointV1.MINIMUM_FEATURE_POINT_COUNT:
             return
         reviewed_res = sort_res.view(res.dtype).reshape((res.shape[0], 4))
-        slice_res = reviewed_res[-50:]
+        slice_res = reviewed_res[-(HipJointV1.MINIMUM_FEATURE_POINT_COUNT):]
         print("Name : ", name, "Mat Shape :", slice_res.shape)
-        reshape_res = slice_res.reshape((200, ))
+        reshape_res = slice_res.reshape((HipJointV1.MINIMUM_FEATURE_POINT_COUNT * 4, ))
         print("Reshaped shape : ", reshape_res.shape)
         self.all_maps[name] = reshape_res
 
@@ -179,7 +183,7 @@ class HipJointV1(object):
 
 
 if __name__ == "__main__":
-    hv1 = HipJointV1(50 * 4, 3)
+    hv1 = HipJointV1(HipJointV1.MINIMUM_FEATURE_POINT_COUNT * 4, 3)
     ## If model exists, load model
     model = hv1.initialize()
 
@@ -197,7 +201,7 @@ if __name__ == "__main__":
     random_labels = hv1.get_predict_labels()
 
     ## 4. Fit
-    hv1.fit_model(model, fit_data, fit_labels)
+    hv1.fit_model(model, fit_data, fit_labels, HipJointV1.EPOCH_TRIES)
     # hv1.fit_model(model, rand_data.get("data"), rand_data.get("labels"))
 
     ## 5. Predict, calculate loss
